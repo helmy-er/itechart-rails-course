c96b39e532bb5daf5091da0e51954ce144b61514
helmy-er <helbmerliuni@gmail.com>
2021-12-03 18:55:45 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  get 'persons/profile'
+  resources :people
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
