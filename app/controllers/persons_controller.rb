bf91a62da96f54bad5b56543916ff6296376660f
helmy-er <helbmerliuni@gmail.com>
2021-12-03 20:53:39 +0300

    add persons

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,6 @@
 class PersonsController < ApplicationController
+
   def profile
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_154905) do
+ActiveRecord::Schema.define(version: 2021_12_03_173837) do
 
   create_table "people", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
