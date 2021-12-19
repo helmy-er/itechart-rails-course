71ea3613b7f5255fb1a7ea346baa62d434d8aa2e
helmy-er <helbmerliuni@gmail.com>
2021-12-19 20:20:40 +0300

    final commit

--- test/controllers/persons_controller_test.rb
+++ test/controllers/persons_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PersonsControllerTest < ActionDispatch::IntegrationTest
-  test "should get profile" do
+  test 'should get profile' do
     get persons_profile_url
     assert_response :success
   end
--- test/system/expenses_test.rb
+++ test/system/expenses_test.rb
@@ -1,41 +1,43 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class ExpensesTest < ApplicationSystemTestCase
   setup do
     @expense = expenses(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit expenses_url
-    assert_selector "h1", text: "Expenses"
+    assert_selector 'h1', text: 'Expenses'
   end
 
-  test "creating a Expense" do
+  test 'creating a Expense' do
     visit expenses_url
-    click_on "New Expense"
+    click_on 'New Expense'
 
-    click_on "Create Expense"
+    click_on 'Create Expense'
 
-    assert_text "Expense was successfully created"
-    click_on "Back"
+    assert_text 'Expense was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Expense" do
+  test 'updating a Expense' do
     visit expenses_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    click_on "Update Expense"
+    click_on 'Update Expense'
 
-    assert_text "Expense was successfully updated"
-    click_on "Back"
+    assert_text 'Expense was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Expense" do
+  test 'destroying a Expense' do
     visit expenses_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Expense was successfully destroyed"
+    assert_text 'Expense was successfully destroyed'
   end
 end
