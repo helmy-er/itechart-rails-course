5b96e8db53b56200a6f785f952b88fed4e21cacb
helmy-er <helbmerliuni@gmail.com>
2022-01-13 12:14:32 +0300

    change user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, length: { maximum: 20 }
 end
